18390c262b828ae2562d258a94c34ca15d74c4c8
hegon910 <4poise@gmail.com>
2025-05-14 11:36:13 +0900

    [Feat] Make_PlayerStatus

---
+++ Assets/Script/Player/PlayerStatus.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DesignPattern;
+public class PlayerStatus : MonoBehaviour
+{
+    [field: SerializeField][field : Range(0, 10)]
+    public float WalkSpeed { get; set; }
+
+    [field: SerializeField][field: Range(0, 10)]
+    public float RunSpeed { get; set; }
+
+    [field: SerializeField][field:Range(0,10)]
+    public float RotateSpeed { get; set; }
+
+    public ObservableProperty<bool> IsAming { get; private set;} = new();
+    public ObservableProperty<bool> IsMoving { get; private set; } = new();
+    public ObservableProperty<bool> IsAttacking { get; private set; } = new();
+
+
+}
